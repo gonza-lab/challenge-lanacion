8f43397978a85fac425e68592562be6b54158ef2
Gonzalo Flores <gonzaflores358@gmail.com>
2025-03-17 10:28:12 -0300

    implement Article and TagList molecules

---
+++ src/components/molecules/Article.server.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react'
+import Typography from '../atoms/Typography.server'
+
+export interface ArticleProps {
+  title: string | ReactNode
+  date: string
+  image: string
+}
+
+export default function Article({ title, image, date }: ArticleProps) {
+  return (
+    <article className="mod-caja-nota living w-100-mobile">
+      <section id="" className="cont-figure">
+        <a href="" className="figure">
+          <picture id="" className="content-pic picture">
+            <img src={image} alt="" className="content-img" />
+          </picture>
+        </a>
+      </section>
+      <div className="mod-caja-nota__descrip lugares">
+        <Typography variant="articleTitle" as="h2">
+          <a href="">{title}</a>
+        </Typography>
+				<Typography variant='date' as='h4'>{date}</Typography>
+      </div>
+    </article>
+  )
+}
---
+++ src/components/molecules/TagList.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Tag, { TagProps } from '../atoms/Tag.server'
+
+interface TagListProps {
+  tags: TagProps[]
+}
+
+export default function TagList({ tags }: TagListProps) {
+  return (
+    <div className="cont_tags hlp-marginBottom-20 com-secondary-tag">
+      {tags.map((tag, index) => (
+        <Tag key={index} {...tag} />
+      ))}
+    </div>
+  )
+}
